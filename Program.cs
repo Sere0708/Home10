207e5ddc0c6fefeb3bd3d0e0eda9db9656a7b209
Sere0708 <103060203+Sere0708@users.noreply.github.com>
2022-06-06 23:10:15 +0300

    .\Home10\

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//. Задача. 1
+
+/*
+Console.WriteLine("Введите текст:");
+string[] textMass;
+string text = Console.ReadLine();
+textMass = text.Split(' ');
+Console.WriteLine("Количество слов:");
+Console.WriteLine(textMass.Length);
+Console.ReadLine();
+
+*/
+
+
+
+
+
+
+
+/*
+ string [] jj={"qwe", "wer", "ert", "rty", "tyu", "yui"};
+ String j="" ;j= String.Join(j, jj, 0, 6);
+ Console.WriteLine(j);
+*/
+
+
+
+
+
+//. Задача 2
+/*
+
+string s1 = "qwe";
+string s2 = "wer";
+string s3 = s1 + "" + s2; // результат: строка "qwewer"
+string s4 = string.Concat(s3, "!!!"); 
+ 
+Console.WriteLine(s4);
+
+*/
